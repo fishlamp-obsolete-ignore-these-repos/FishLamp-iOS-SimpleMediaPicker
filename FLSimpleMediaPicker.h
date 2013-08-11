ea53361adb2c6cecb79bcb6f2acf36510f935c23
Mike Fullerton <mike@greentongue.com>
2013-08-10 20:07:58 -0700

    move up one

---
+++ FLSimpleMediaPicker.h
@@ -0,0 +1,61 @@
+//
+//	FLMediaPicker.h
+//	FishLamp
+//
+//	Created by Mike Fullerton on 9/5/09.
+//	Copyright (c) 2013 GreenTongue Software LLC, Mike Fullerton. 
+//  The FishLamp Framework is released under the MIT License: http://fishlamp.com/license 
+//
+
+#import "FLImageAsset.h"
+#import "FLLegacyProgressView.h"
+#import "FLObservable.h"
+#import "FLSimplePhoto.h"
+
+#if USE_POPOVERS
+#import "FLModalPopoverController.h"
+#endif
+
+@interface FLSimpleMediaPicker : FLObservable<UIImagePickerControllerDelegate,
+									UINavigationControllerDelegate,
+									UIPopoverControllerDelegate> {
+	UIImagePickerController* _picker;
+
+#if USE_POPOVERS
+	FLModalPopoverController* _popoverController;
+#endif
+    __unsafe_unretained UIViewController* _viewController;
+	
+	NSUInteger _pickedCount;
+	BOOL _animated;
+    BOOL _hidden;
+}
+
+@property (readonly, assign, nonatomic) NSUInteger pickedCount;
+
+- (void) presentModallyInViewController:(UIViewController*) viewController
+                               animated:(BOOL) animated;
+
+- (void) hideMediaPicker;
+
+#if USE_POPOVERS
+// e.g. apple's popovers in their sdk (not a fan of these).
+
+- (FLModalPopoverController*) presentModallyInPopoverFromViewController:(UIViewController*) viewController
+                                                                animate:(BOOL) animated;
+
+- (FLModalPopoverController*) presentInPopoverFromViewController:(UIViewController*) viewController
+                                        permittedArrowDirections:(UIPopoverArrowDirection)arrowDirections
+                                                        fromRect:(CGRect) rect
+                                                         animate:(BOOL) animated;
+#endif
+@end
+
+@protocol FLSimpleMediaPickerObserver <FLObserver>
+@optional
+
+- (void) simpleMediaPicker:(FLSimpleMediaPicker*) picker
+              didPickPhoto:(FLSimplePhoto*) photo;
+
+- (void) simpleMediaPickerDidClose:(FLSimpleMediaPicker*) picker;
+@end
